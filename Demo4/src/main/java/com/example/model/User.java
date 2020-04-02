bffaf4c58c07f361dd318113a8d4f5e299d54077
DELL <DELL@DESKTOP-O5B4F52>
2020-04-02 12:40:26 +0530

    first commit

---
+++ Demo4/src/main/java/com/example/model/User.java
@@ -0,0 +1,45 @@
+package com.example.model;
+
+public class User {
+	private int id;
+	private String name;
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	@Override
+	public String toString() {
+		return "User [id=" + id + ", name=" + name + "]";
+	}
+	public User(int id, String name) {
+		super();
+		this.id = id;
+		this.name = name;
+	}
+	public User() {
+		super();
+	}
+	
+	
+	
+
+}
+
+
+
+
+/*spring.datasource.url=jdbc:postgres://locahost:5432/Mydb
+
+spring.datasource.data-username=postgresSQL
+
+spring.datasource.data-password=manager
+	
+spring.jpa.properties.hibernate.dialect = org.hibernate.dialect.PostgresDialect*/
---
+++ Demo4/src/main/webapp/view/home.jsp
@@ -0,0 +1,23 @@
+
+<%@ page language="java" contentType="text/html; charset=ISO-8859-1"
+    pageEncoding="ISO-8859-1"%>
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="ISO-8859-1">
+<title>Insert title here</title>
+</head>
+<body>
+Welcome in spring Boot MVC
+
+
+
+<form action="add" method="post">
+Enter Id <input type="text" name="id"><br>
+Enter Name <input type="text" name="name"><br>
+<input type="submit">
+</form>
+
+Welcome in spring Boot MVC
+</body>
+</html>
